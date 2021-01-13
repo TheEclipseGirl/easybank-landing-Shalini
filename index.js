43155b95dc7c0e9680d9bf50d4d5166ea02a8fdb
Shalini <shalinibvp.02@gmail.com>
2021-01-13 17:51:59 +0530

    active states added

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 $(".hamburger-pop-up").hide();
+$("#overlay").hide();
 
 function toggleHamburgerClick(){
     if($("#hamburger").attr("src")==="./images/icon-hamburger.svg"){
         $("#hamburger").attr("src", "./images/icon-close.svg" )
         $(".hamburger-pop-up").slideDown();
+        $("#overlay").show();
     }
-
-
     else{
         $("#hamburger").attr("src", "./images/icon-hamburger.svg" )
         $(".hamburger-pop-up").slideUp();
-
+        $("#overlay").hide();
     }
-
-
 }
 
